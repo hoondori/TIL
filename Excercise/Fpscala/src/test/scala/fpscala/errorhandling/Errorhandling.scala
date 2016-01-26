06f217d5a7c9fe0668084135d4516ee2bcba0797
seonghoon jung <hoondori@seonghoonui-MacBook.local>
2016-01-26 20:04:02 +0900

    commit

--- Excercise/Fpscala/src/test/scala/fpscala/errorhandling/Errorhandling.scala
+++ Excercise/Fpscala/src/test/scala/fpscala/errorhandling/Errorhandling.scala
@@ -130,6 +130,17 @@ class Errorhandling extends FlatSpec with Matchers {
 
   }
 
+  "Exercise 4.8" should "do" in {
+
+    /*In this implementation, map2 is only able to report one error, even if both the name and the age are invalid.
+    What would you need to change in order to report both errors?
+    Would you change map2 or the signature of mkPerson?
+      Or could you create a new data type that captures this requirement better than Either does,
+    with some additional structure? How would orElse, traverse, and sequence behave differently for that data type?*/
+
+
+  }
+
 
   // convert ordinary function to Option-adapted function
   def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
