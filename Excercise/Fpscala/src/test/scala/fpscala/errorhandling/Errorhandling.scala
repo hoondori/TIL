37d8ec033a0a79fcf85d15cd7a7b6d4fe27e4fa8
seonghoon jung <hoondori@seonghoonui-MacBook.local>
2016-01-26 19:57:17 +0900

    commit

--- Excercise/Fpscala/src/test/scala/fpscala/errorhandling/Errorhandling.scala
+++ Excercise/Fpscala/src/test/scala/fpscala/errorhandling/Errorhandling.scala
@@ -111,15 +111,22 @@ class Errorhandling extends FlatSpec with Matchers {
       sequence( a map (i => f(i) ) )
 
     def traverse[A,B](a: List[A])(f: A => Option[B]): Option[List[B]] =
-      a.foldRight[Option[List[B]]](Some(List[B]()))((x,y) => f(x)::y)
+      a.foldRight[Option[List[B]]](Some(Nil))((x,y) => map2(f(x),y)(_::_))
 
+    def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] =
+      traverse(a)( x => x )
   }
 
   "Exercise 4.6" should "do" in {
 
+    /*Implement versions of map, flatMap, orElse, and map2 on Either that operate on the Right value*/
+
+
   }
 
   "Exercise 4.7" should "do" in {
+
+    /*Implement sequence and traverse for Either. These should return the first error that’s encountered, if there is one.*/
 
   }
 
@@ -163,3 +170,43 @@ sealed trait Option[+A] {
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
+sealed trait Either[+E,+A] {
+  def map[B](f: A => B): Either[E,B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => Right(f(a))
+  }
+  def flatMap[EE >:E, B](f: A => Either[EE,B]): Either[EE,B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
+  }
+  def orElse[EE >:E, B >: A](b: => Either[EE,B]): Either[EE,B] = this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
+
+  def map2[EE >:E,B,C](b: Either[EE,B])(f: (A,B) => C): Either[EE,C] =
+    this flatMap { aa =>
+      b map { bb =>
+        f(aa,bb)
+      }
+    }
+
+  def map2_1[EE >:E,B,C](b: Either[EE,B])(f: (A,B) => C): Either[EE,C] =
+    for {
+      aa <- this
+      bb <- b
+    } yield f(aa, bb)
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    es.foldRight[Either[E,List[A]]](Right(Nil))((x,y) => x.map2(y)(_::_))
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    as.foldRight[Either[E,List[B]]](Right(Nil))((x,y) => f(x).map2(y)(_::_))
+
+  def sequenceViaTraverse[E,A](es: List[Either[E,A]]): Either[E, List[A]] =
+    traverse(es)( x => x )
+
+}
+
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing,A]
