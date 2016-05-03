39692ecd961500037b473e91c66eb473c5a877f9
hoondori <hoondori@APPLEui-iMac.local>
2016-05-04 05:09:34 +0900

    commit

--- Excercise/tensorflow/4_deep/8_nn_dropout_mnist.py
+++ Excercise/tensorflow/4_deep/8_nn_dropout_mnist.py
@@ -27,13 +27,13 @@ Y = tf.placeholder("float", [None, 10])
 # Set model weights
 W1 = tf.get_variable("W1", shape=[784,256], initializer=xavier_init(784,256))
 W2 = tf.get_variable("W2", shape=[256,256], initializer=xavier_init(256,256))
-W3 = tf.get_variable("W3", shape=[256,256], initializer=xavier_init(256,256))
-W4 = tf.get_variable("W4", shape=[256,256], initializer=xavier_init(256,256))
-W5 = tf.get_variable("W5", shape=[256,10], initializer=xavier_init(256,10))
+W3 = tf.get_variable("W3", shape=[256,128], initializer=xavier_init(256,128))
+W4 = tf.get_variable("W4", shape=[128,64], initializer=xavier_init(128,64))
+W5 = tf.get_variable("W5", shape=[64,10], initializer=xavier_init(64,10))
 B1 = tf.Variable(tf.random_normal([256]), name="Bias1")
 B2 = tf.Variable(tf.random_normal([256]), name="Bias2")
-B3 = tf.Variable(tf.random_normal([256]), name="Bias3")
-B4 = tf.Variable(tf.random_normal([256]), name="Bias4")
+B3 = tf.Variable(tf.random_normal([128]), name="Bias3")
+B4 = tf.Variable(tf.random_normal([64]), name="Bias4")
 B5 = tf.Variable(tf.random_normal([10]), name="Bias5")
 
 # Construct model
